e6796760259404bc2f24b5a3063e696b49f3193f
Flavio Böni <6606236+flaviolev@users.noreply.github.com>
2021-04-06 23:27:32 +0200

    fix(landingpage): use capital letter for app file

--- apps/client/src/index.tsx
+++ apps/client/src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
 import './i18n';
-import App from './app';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
