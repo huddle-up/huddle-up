07e7697eeb9c289d357da356f74673793c558b01
Severin Hauser <severin@tilted.ch>
2021-04-06 16:34:25 +0200

    feat(api): add database setup

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/app/config.module';
+import { DatabaseConfigModule } from './config/database/config.module';
+import { DatabaseConfigService } from './config/database/config.service';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [
+    AppConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [DatabaseConfigModule],
+      inject: [DatabaseConfigService],
+      useFactory: async (dbConfigService: DatabaseConfigService) => ({
+        type: 'postgres',
+        username: dbConfigService.username,
+        password: dbConfigService.password,
+        database: dbConfigService.database,
+        host: dbConfigService.host,
+        port: dbConfigService.port,
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
