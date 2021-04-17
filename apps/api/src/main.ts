76dd16a8f930d3a0d20466536f75c8394f017460
Severin Hauser <severin@tilted.ch>
2021-04-17 11:30:54 +0200

    feat(api): add cors and helmet

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config/app/config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   const appConfig: AppConfigService = app.get('AppConfigService');
 
