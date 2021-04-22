2c7efb60af49173b4f0de6c806794352c52f5471
Severin Hauser <severin@tilted.ch>
2021-04-22 09:40:39 +0200

    fix(api): include userId in current user decorator

--- apps/api/src/meetings/meetings.module.ts
+++ apps/api/src/meetings/meetings.module.ts
@@ -3,10 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MeetingsService } from './meetings.service';
 import { MeetingsResolver } from './meetings.resolver';
 import { Meeting } from './entities/meeting.entity';
-import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forFeature([Meeting])],
+  imports: [TypeOrmModule.forFeature([Meeting])],
   providers: [MeetingsResolver, MeetingsService],
 })
 export class MeetingsModule {}
