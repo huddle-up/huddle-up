c018d5dee4583c20b06f95f5195f54e5aa1d2ef5
Flavio Böni <6606236+flaviolev@users.noreply.github.com>
2021-04-24 16:31:07 +0200

    feat(client): add clientside filters

--- apps/client/src/pages/meetings/meetings-page.tsx
+++ apps/client/src/pages/meetings/meetings-page.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useQuery } from '@apollo/client';
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 import { MeetingList } from '../../components/meeting';
 import { Meetings, MeetingsVariables } from '../../models/meetings/__generated-interfaces__/Meetings';
 import SearchField from '../../components/search/search-field';
 import { MEETINGS } from '../../models/meetings';
 import { AppPageAside, AppPageMain } from '../../components/app-page-layout';
 import { SectionHeader } from '../../components/section-header';
+import { isInFutureFilter, isWithinIntervalFilter } from '../../utils';
 
 function MeetingsPage() {
   const { t } = useTranslation();
@@ -37,8 +39,10 @@ function MeetingsPage() {
       </AppPageAside>
       <AppPageMain>
         <section>
-          <SectionHeader icon={<PlayCircleOutlineIcon />} title={t('global.title.meetings')} />
-          <MeetingList meetings={data.meetings} />
+          <SectionHeader icon={<PlayCircleOutlineIcon />} title={t('meetings.title.ongoing')} />
+          <MeetingList meetings={isWithinIntervalFilter(data.meetings)} />
+          <SectionHeader icon={<CalendarTodayIcon />} title={t('meetings.title.upcoming')} />
+          <MeetingList meetings={isInFutureFilter(data.meetings)} />
         </section>
       </AppPageMain>
     </>
