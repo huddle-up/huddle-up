6265361ce584eb7fa3964cf3d4fcc724d02555ea
Severin Hauser <severin@tilted.ch>
2021-05-01 17:00:48 +0200

    feat(conference): add conference control flow

--- apps/client/src/components/conference-status/conference-status.tsx
+++ apps/client/src/components/conference-status/conference-status.tsx
@@ -1,7 +1,8 @@
 import { Button, CircularProgress, makeStyles, Typography } from '@material-ui/core';
-import { PersonAdd, PlayArrow, Schedule } from '@material-ui/icons';
+import { EventBusy, PersonAdd, PlayArrow, Schedule, Stop, Tune } from '@material-ui/icons';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { MeetingFields } from '../../models/meetings/__generated-interfaces__/MeetingFields';
 import { useConferenceStatus } from './use-conference-status';
 
 const useStyles = makeStyles((theme) => ({
@@ -9,20 +10,17 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     alignItems: 'center',
     padding: theme.spacing(0.5),
-  },
-  pendingIcon: {
-    marginRight: theme.spacing(1),
+    paddingRight: 0,
+    '& > :not(:last-child)': {
+      marginRight: theme.spacing(1),
+    },
   },
 }));
 
 interface ConferenceStatusProps {
-  meeting: {
-    id: string;
-    host: {
-      id: string;
-    };
-  };
+  meeting: MeetingFields;
 }
+
 function StartConferenceContent({ onCreate }: { onCreate: () => void }) {
   const { t } = useTranslation();
   return (
@@ -32,15 +30,73 @@ function StartConferenceContent({ onCreate }: { onCreate: () => void }) {
   );
 }
 
-function ConferencePendingContent() {
+function SetupConferenceContent({ url, onPublish }: { url: string; onPublish: () => void }) {
   const { t } = useTranslation();
-  const classes = useStyles();
   return (
     <>
-      <Schedule className={classes.pendingIcon} />
+      <Button
+        href={url}
+        target="_BLANK"
+        rel="noreferrer noopener"
+        variant="outlined"
+        color="secondary"
+        disableElevation
+        startIcon={<Tune />}>
+        {t('meetings.conference.setup')}
+      </Button>
+      <Button
+        onClick={onPublish}
+        type="button"
+        variant="contained"
+        color="secondary"
+        disableElevation
+        startIcon={<PlayArrow />}>
+        {t('meetings.conference.publish')}
+      </Button>
+    </>
+  );
+}
+
+function ConferencePendingContent({ label }: { label: string }) {
+  return (
+    <>
+      <Schedule />
       <Typography component="span" variant="body2">
-        {t('meetings.conference.pending')}
+        {label}
       </Typography>
+    </>
+  );
+}
+
+function ConferenceEndedContent() {
+  const { t } = useTranslation();
+  return (
+    <>
+      <EventBusy />
+      <Typography component="span" variant="body2">
+        {t('meetings.conference.ended')}
+      </Typography>
+    </>
+  );
+}
+
+function ManageConferenceContent({ url, onStop }: { url: string; onStop: () => void }) {
+  const { t } = useTranslation();
+  return (
+    <>
+      <Button onClick={onStop} type="button" variant="outlined" color="secondary" disableElevation startIcon={<Stop />}>
+        {t('meetings.conference.stop')}
+      </Button>
+      <Button
+        href={url}
+        target="_BLANK"
+        rel="noreferrer noopener"
+        variant="contained"
+        color="secondary"
+        disableElevation
+        startIcon={<PersonAdd />}>
+        {t('meetings.conference.join')}
+      </Button>
     </>
   );
 }
@@ -63,13 +119,18 @@ function JoinConferenceContent({ url }: { url: string }) {
 
 function ConferenceStatus({ meeting }: ConferenceStatusProps) {
   const classes = useStyles();
-  const { display, createConference, access } = useConferenceStatus({ meeting });
+  const { t } = useTranslation();
+  const { display, createConference, publishConference, stopConference, access } = useConferenceStatus({ meeting });
   return (
     <div className={classes.root}>
       {display === 'loading' && <CircularProgress size="2em" />}
-      {display === 'pending' && <ConferencePendingContent />}
+      {display === 'early' && <ConferencePendingContent label={t('meetings.conference.early')} />}
+      {display === 'pending' && <ConferencePendingContent label={t('meetings.conference.pending')} />}
       {display === 'start' && <StartConferenceContent onCreate={createConference} />}
+      {display === 'setup' && access && <SetupConferenceContent url={access.url} onPublish={publishConference} />}
+      {display === 'manage' && access && <ManageConferenceContent url={access.url} onStop={stopConference} />}
       {display === 'join' && access && <JoinConferenceContent url={access.url} />}
+      {display === 'ended' && <ConferenceEndedContent />}
     </div>
   );
 }
--- apps/client/src/components/conference-status/use-conference-status.ts
+++ apps/client/src/components/conference-status/use-conference-status.ts
@@ -1,30 +1,113 @@
 import { useLazyQuery, useMutation, useQuery } from '@apollo/client';
+import { isFuture, parseISO } from 'date-fns';
 import { useEffect, useMemo } from 'react';
-import { CONFERENCE_ACCESS, CONFERENCE_BY_MEETING, CREATE_CONFERENCE } from '../../models/conferences';
+import {
+  CONFERENCE_ACCESS,
+  CONFERENCE_BY_MEETING,
+  CREATE_CONFERENCE,
+  PUBLISH_CONFERENCE,
+  STOP_CONFERENCE,
+} from '../../models/conferences';
 import { ConferenceAccess } from '../../models/conferences/__generated-interfaces__/ConferenceAccess';
 import { ConferenceByMeeting } from '../../models/conferences/__generated-interfaces__/ConferenceByMeeting';
 import {
   CreateConference,
   CreateConferenceVariables,
 } from '../../models/conferences/__generated-interfaces__/CreateConference';
+import {
+  PublishConference,
+  PublishConferenceVariables,
+} from '../../models/conferences/__generated-interfaces__/PublishConference';
+import {
+  StopConference,
+  StopConferenceVariables,
+} from '../../models/conferences/__generated-interfaces__/StopConference';
+import { MeetingFields } from '../../models/meetings/__generated-interfaces__/MeetingFields';
 import { useUser } from '../../models/user';
 
-type ConferenceState = 'loading' | 'error' | 'void' | 'created' | 'started' | 'published' | 'stopped';
+/**
+ * The state of the conference
+ */
+type ConferenceState =
+  /**
+   * The conference is loading
+   */
+  | 'loading'
+  /**
+   * The conference could not be loaded, or contains an error
+   */
+  | 'error'
+  /**
+   * The conference does not exist
+   */
+  | 'void'
+  /**
+   * The conference does not exist, but can be created by the host
+   */
+  | 'ready'
+  /**
+   * The conference has been created, but not opened to all participants
+   */
+  | 'created'
+  /**
+   * The conference has been started and is open to all participants
+   */
+  | 'published'
+  /**
+   * The conference has been stopped
+   */
+  | 'stopped';
 
-type DisplayState = 'loading' | 'error' | 'pending' | 'start' | 'setup' | 'join' | 'ended';
+/**
+ * How the conference state should be displayed to the current user
+ */
+type DisplayState =
+  /**
+   * Something is loading
+   */
+  | 'loading'
+  /**
+   * An error has been encountered
+   */
+  | 'error'
+  /**
+   * The user is the host, but the conference cannot be created, yet
+   */
+  | 'early'
+  /**
+   * The user is not the host and the conference has not been published, yet
+   */
+  | 'pending'
+  /**
+   * The user is the host and the conference can be started
+   */
+  | 'start'
+  /**
+   * The user is the host and the conference has been created, but is not published, yet
+   */
+  | 'setup'
+  /**
+   * The user is the host and the conference is published
+   */
+  | 'manage'
+  /**
+   * The user is not the host, the conference is created and can be joined
+   */
+  | 'join'
+  /**
+   * The conference has ended
+   */
+  | 'ended';
 
+type ConferenceRole = 'participant' | 'host';
 interface UseConferenceStatusProps {
-  meeting: {
-    id: string;
-    host: {
-      id: string;
-    };
-  };
+  meeting: MeetingFields;
 }
 
-function useConferenceQuery(meetingId: string) {
+function useConferenceQuery(meetingId: string, pollFrequency = 5) {
   const { loading, error, data } = useQuery<ConferenceByMeeting>(CONFERENCE_BY_MEETING, {
     variables: { meetingId },
+    pollInterval: pollFrequency * 1000,
   });
 
   return {
@@ -41,12 +124,46 @@ function useCreateConference(meetingId: string) {
 
   const createConference = () => {
     if (!called) {
-      mutate({ variables: { meetingId }, refetchQueries: ['ConferenceByMeeting'] });
+      mutate({ variables: { meetingId } });
     }
   };
 
   return {
     createConference,
+    called,
+    loading,
+    error,
+  };
+}
+
+function usePublishConference(conferenceId?: string) {
+  const [mutate, { called, loading, error }] = useMutation<PublishConference, PublishConferenceVariables>(
+    PUBLISH_CONFERENCE
+  );
+
+  const publishConference = () => {
+    if (conferenceId) {
+      mutate({ variables: { conferenceId } });
+    }
+  };
+  return {
+    publishConference,
+    called,
+    loading,
+    error,
+  };
+}
+
+function useStopConference(conferenceId?: string) {
+  const [mutate, { called, loading, error }] = useMutation<StopConference, StopConferenceVariables>(STOP_CONFERENCE);
+
+  const stopConference = () => {
+    if (conferenceId) {
+      mutate({ variables: { conferenceId } });
+    }
+  };
+  return {
+    stopConference,
     called,
     loading,
     error,
@@ -71,10 +188,33 @@ function useConferenceAccessQuery(conferenceId?: string) {
   };
 }
 
+const roleDisplayMapping: Record<ConferenceRole, Record<ConferenceState, DisplayState>> = {
+  participant: {
+    loading: 'loading',
+    error: 'error',
+    void: 'pending',
+    created: 'pending',
+    ready: 'pending',
+    published: 'join',
+    stopped: 'ended',
+  },
+  host: {
+    loading: 'loading',
+    error: 'error',
+    void: 'early',
+    ready: 'start',
+    created: 'setup',
+    published: 'manage',
+    stopped: 'ended',
+  },
+};
+
 export function useConferenceStatus({ meeting }: UseConferenceStatusProps) {
   const { user } = useUser();
   const { conference, ...conferenceQuery } = useConferenceQuery(meeting.id);
   const { createConference } = useCreateConference(meeting.id);
+  const { publishConference } = usePublishConference(conference ? conference.id : undefined);
+  const { stopConference } = useStopConference(conference ? conference.id : undefined);
   const { access, ...accessQuery } = useConferenceAccessQuery(conference ? conference.id : undefined);
   const isHost = meeting.host.id === user.id;
 
@@ -85,24 +225,27 @@ export function useConferenceStatus({ meeting }: UseConferenceStatusProps) {
     if (conferenceQuery.loading || accessQuery.loading) {
       return 'loading';
     }
-    if (!conference) {
+    if (isFuture(parseISO(meeting.prepareDate))) {
       return 'void';
     }
+    if (!conference) {
+      return 'ready';
+    }
+    if (conference.stoppedAt) {
+      return 'stopped';
+    }
+    if (conference.publishedAt) {
+      return 'published';
+    }
     return 'created';
-  }, [conference, conferenceQuery, accessQuery]);
+  }, [conference, conferenceQuery, accessQuery, meeting]);
 
   const display: DisplayState = useMemo(() => {
-    switch (state) {
-      case 'void':
-        return isHost ? 'start' : 'pending';
-      case 'created':
-        return 'join';
-      case 'error':
-        return 'error';
-      case 'loading':
-      default:
-        return 'loading';
+    const role = isHost ? 'host' : 'participant';
+    if (roleDisplayMapping[role] && roleDisplayMapping[role][state]) {
+      return roleDisplayMapping[role][state];
     }
+    return 'loading';
   }, [state, isHost]);
 
   return {
@@ -111,5 +254,7 @@ export function useConferenceStatus({ meeting }: UseConferenceStatusProps) {
     access,
     display,
     createConference,
+    publishConference,
+    stopConference,
   };
 }
