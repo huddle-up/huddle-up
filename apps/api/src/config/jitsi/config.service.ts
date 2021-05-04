c8486170bcb49a4ba79dcfb1c3b58a35a40b3c01
Severin Hauser <severin@tilted.ch>
2021-05-04 11:52:16 +0200

    feat(api): add jwt functionality to jitsi service

--- apps/api/src/config/jitsi/config.service.ts
+++ apps/api/src/config/jitsi/config.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
+interface JitsiJwtConfig {
+  enabled: boolean;
+  appId: string;
+  secret: string;
+}
+
 @Injectable()
 export class JitsiConfigService {
   constructor(private configService: ConfigService) {}
 
   get host(): string {
     return this.configService.get<string>('jitsi.host');
+  }
+
+  get jwt(): JitsiJwtConfig {
+    return this.configService.get<JitsiJwtConfig>('jitsi.jwt');
   }
 }
--- apps/api/src/config/jitsi/configuration.ts
+++ apps/api/src/config/jitsi/configuration.ts
@@ -2,4 +2,9 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('jitsi', () => ({
   host: process.env.HU_JITSI_HOST,
+  jwt: {
+    enabled: process.env.HU_JITSI_JWT_ENABLED || false,
+    appId: process.env.HU_JITSI_JWT_APP_ID || '',
+    secret: process.env.HU_JITSI_JWT_SECRET || '',
+  },
 }));
