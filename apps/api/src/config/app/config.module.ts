080d8edc5881ce1ccd65bba463d0c77c30cc2af3
Severin Hauser <severin@tilted.ch>
2021-05-19 15:28:55 +0200

    refactor(api): move configuration to base directory

--- apps/api/src/config/app/config.module.ts
+++ apps/api/src/config/app/config.module.ts
@@ -1,12 +1,19 @@
 import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { resolve, join } from 'path';
 import configuration from './configuration';
 import { AppConfigService } from './config.service';
+
+function envPaths() {
+  const configDirectory = resolve(__dirname, '../../../../../config/api');
+  return [join(configDirectory, '.env')];
+}
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      envFilePath: envPaths(),
       load: [configuration],
       validationSchema: Joi.object({
         HU_PORT: Joi.number().default(4000),
