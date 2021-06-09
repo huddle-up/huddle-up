f5c9fab374d1f1a360db193164f277c9976f2a0f
Severin Hauser <severin@tilted.ch>
2021-06-09 17:09:26 +0200

    fix(client): disable updates once meeting is canceled, or started

--- apps/client/src/pages/meetings/meeting-update-page.tsx
+++ apps/client/src/pages/meetings/meeting-update-page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Box, Card, CardContent, Grid, Typography } from '@material-ui/core';
 import { useQuery, useMutation } from '@apollo/client';
 import { Redirect, useParams } from 'react-router-dom';
-import { Cancel, PlayCircleOutline } from '@material-ui/icons';
+import { Cancel, Info, PlayCircleOutline } from '@material-ui/icons';
 import { Meeting, MeetingVariables } from '../../models/meetings/__generated-interfaces__/Meeting';
 import { UpdateMeeting, UpdateMeetingVariables } from '../../models/meetings/__generated-interfaces__/UpdateMeeting';
-import { isHost, MEETING, UPDATE_MEETING } from '../../models/meetings';
+import { isHost, MEETING, MeetingState, UPDATE_MEETING, useWatchMeetingState } from '../../models/meetings';
 import { AppPageMain } from '../../components/app-page-layout';
 import { SectionHeader } from '../../components/section-header';
 import { CancelMeetingForm, UpdateMeetingForm } from '../../components/meeting-form';
@@ -16,6 +17,25 @@ import { generateLink, ROUTES } from '../../routes';
 import { ErrorCard } from '../../components/error';
 import { LoadingContent } from '../../components/loading';
 import { useUser } from '../../models/user';
+
+function InfoCard({ text }: { text: string }) {
+  return (
+    <Box mb={2}>
+      <Card variant="outlined">
+        <CardContent>
+          <Grid container spacing={1}>
+            <Grid item>
+              <Info color="disabled" />
+            </Grid>
+            <Grid item>
+              <Typography component="p">{text}</Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+}
 
 function MeetingUpdatePage() {
   const { t } = useTranslation();
@@ -30,6 +50,9 @@ function MeetingUpdatePage() {
     UpdateMeeting,
     UpdateMeetingVariables
   >(UPDATE_MEETING);
+
+  const meetingState = useWatchMeetingState(data?.meeting, user);
+  const canEdit = [MeetingState.Future, MeetingState.ReadyToStart].includes(meetingState);
 
   async function handleMeetingUpdate(meetingVariables: UpdateMeetingVariables) {
     const tags: TagOption[] = meetingVariables.tags?.map((tag) => {
@@ -70,11 +93,12 @@ function MeetingUpdatePage() {
           {mutationError && <ErrorCard detail={mutationError.message} />}
           {!mutationLoading && !mutationError && mutationCalled && <Redirect to={`/meetings/${id}`} />}
 
-          <UpdateMeetingForm onSubmit={handleMeetingUpdate} initialValues={meeting} />
+          {!canEdit && <InfoCard text={t('meetings.form.noMoreUpdate')} />}
+          <UpdateMeetingForm onSubmit={handleMeetingUpdate} initialValues={meeting} disabled={!canEdit} />
         </section>
         <section>
           <SectionHeader icon={<Cancel />} title={t('meetings.cancel.title')} />
-          <CancelMeetingForm meeting={meeting} />
+          {canEdit ? <CancelMeetingForm meeting={meeting} /> : <InfoCard text={t('meetings.cancel.noMore')} />}
         </section>
       </AppPageMain>
     </>
